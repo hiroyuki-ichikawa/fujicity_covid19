8be3a0d461951f519ab4052efcf1b277eefe6da5
Akira Tanaka <suntomoon@gmail.com>
2020-08-01 01:25:24 +0900

    検査陽性者の状況の分類を追加

--- utils/formatConfirmedCases.ts
+++ utils/formatConfirmedCases.ts
@@ -21,6 +21,18 @@ type DataType = {
           ]
         },
         {
+          attr: '宿泊療養'
+          value: number
+        },
+        {
+          attr: '自宅療養'
+          value: number
+        },
+        {
+          attr: '調査中'
+          value: number
+        },
+        {
           attr: '退院'
           value: number
         },
@@ -39,6 +51,9 @@ type ConfirmedCasesType = {
   入院中: number
   軽症中等症: number
   重症: number
+  宿泊療養: number
+  自宅療養: number
+  調査中: number
   死亡: number
   退院: number
 }
@@ -55,8 +70,11 @@ export default (data: DataType) => {
     入院中: data.children[0].children[0].value,
     軽症中等症: data.children[0].children[0].children[0].value,
     重症: data.children[0].children[0].children[1].value,
-    死亡: data.children[0].children[2].value,
-    退院: data.children[0].children[1].value
+    宿泊療養: data.children[0].children[1].value,
+    自宅療養: data.children[0].children[2].value,
+    調査中: data.children[0].children[3].value,
+    死亡: data.children[0].children[4].value,
+    退院: data.children[0].children[5].value
   }
   return formattedData
 }

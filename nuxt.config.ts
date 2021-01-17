4012371b098d74322f76cf5724468ad3045c4b40
hiroyuki ichikawa <yaritsusozai@gmail.com>
2021-01-17 19:21:01 +0900

    静岡の表記を富士市に変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,7 +15,7 @@ const config: Configuration = {
     htmlAttrs: {
       prefix: 'og: http://ogp.me/ns#'
     },
-    titleTemplate: '%s | 静岡県 新型コロナウイルス感染症対策サイト',
+    titleTemplate: '%s | 富士市 新型コロナウイルス感染症対策サイト',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -23,7 +23,7 @@ const config: Configuration = {
       {
         hid: 'og:url',
         property: 'og:url',
-        content: 'https://stopcovid19.code4numazu.org'
+        content: 'https://stopcovid19-fuji.netlify.app'
       },
       {
         hid: 'twitter:card',
@@ -149,7 +149,7 @@ const config: Configuration = {
     // hardSource: process.env.NODE_ENV === 'development'
   },
   manifest: {
-    name: '静岡県 新型コロナウイルス感染症対策サイト',
+    name: '富士市 新型コロナウイルス感染症対策サイト',
     theme_color: '#00a040',
     background_color: '#ffffff',
     display: 'standalone',
